b9f11650b61c33f0abee7a9b7454eba210d7845b
malkist01 <164641045+malkist01@users.noreply.github.com>
2026-02-01 20:48:55 +0700

    Update upload-assets.sh

--- upload-assets.sh
+++ upload-assets.sh
@@ -4,11 +4,12 @@ tag="$1"
 repo_dir="$2"
 artifacts_dir="$3"
 
+cd $artifacts_dir
+
 CAPTION="Compile Complete, Have A Brick Day"
 BOT_TOKEN="7868194496:AAGY7WwRRbeCOPYOnczoCPh2psC43Q0F3JI"
 CHAT_ID="-1002287610863"
 
-cd $artifacts_dir
 for kernel in *; do
     if [ -d "$kernel" ]; then
        escaped_kernel=$(printf '%q' "$kernel")
@@ -21,7 +22,7 @@ for kernel in *; do
        gh release upload "$tag" "$artifacts_dir/$escaped_kernel/$escaped_kernel.zip"
        echo "--- $escaped_kernel uploaded ---"
        URL="https://api.telegram.org/bot$BOT_TOKEN/sendDocument"
-       curl -s -X POST "$URL" -F document=@"$escaped_kernel" -F caption="$CAPTION" -F chat_id="$CHAT_ID"
+       curl -s -X POST "$URL" -F document=@"$escaped_kernel.zip" -F caption="$CAPTION" -F chat_id="$CHAT_ID"
        cd $artifacts_dir
        rm -rf $kernel
        echo "--- Cleaned up $kernel ---"
